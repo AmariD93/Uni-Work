0aa8673cda6d68947500923488b1ea4acba2e82b
Rustynails93 <68316929+Rustynails93@users.noreply.github.com>
2020-09-18 17:04:12 +1000

    continued creating if else staements and adding some arithmetic for interesting outputs.

--- Week01/src/ifElseHaveAGoodTime.java
+++ Week01/src/ifElseHaveAGoodTime.java
@@ -1,51 +1,84 @@
 public class ifElseHaveAGoodTime {
 	public static void main(String[] args) {
 		GTerm gt = new GTerm(600, 400);
-	
-				
-	
-		String userInput = gt.getInputString("Please enter each using a comma: First name, What is your Gender, How much do you have to spend?, How Much does each beer cost?, Do you have time?- Yes/No");
-	    
-		String [] studentInput = userInput.split(",");
-		
-		String userName = studentInput[0];
-		char userGender.charAt[0] = studentInput[1];
-		int userFunds = Integer.parseInt(studentInput[2]);
-		double beerCost = Double.parseDouble(studentInput[3]);
-		boolean doYouHaveTime = Boolean.parseBoolean (studentInput[4]);
-		
 
-		
-		gt.println("Hey there " + userName);
-		gt.println(""+userGender);
-		gt.println(""+userFunds);
-		gt.println(""+ beerCost);
-		gt.println(""+ doYouHaveTime);
-		
-		
+		String userName = "Daniel";
+		char userGender = 'M';
+		int userFunds = 40;
+		double beerCost = 6.50;
+		boolean doYouHaveTime = true;
+
+		gt.setFontSize(18);
+
 		/*
-		int a = Integer.parseInt(gt.getInputString("select a number betw 1 and 6"));
+		 * String userInput = gt.
+		 * getInputString("Please enter each using a comma: First name, What is your Gender, How much do you have to spend?, How Much does each beer cost?, Do you have time?- Yes/No"
+		 * );
+		 * 
+		 * String [] studentInput = userInput.split(",");
+		 * 
+		 * String userName = studentInput[0]; char userGender = int userFunds =
+		 * Integer.parseInt(studentInput[2]); double beerCost =
+		 * Double.parseDouble(studentInput[3]); boolean doYouHaveTime =
+		 * Boolean.parseBoolean (studentInput[4]);
+		 */
+
+		gt.println("Oi!" + userName + "!");
+
+		// This if else statement shows a greeting depending on the gender of the user
+
+		if (userGender == 'M') {
+			gt.println("How are you mate?");
+
+		}
+		if (userGender == 'F') {
+			gt.println("How are you Gurl!?");
+		}
+
+		else {
+			gt.println("You have entered in something wrong");
+		}
+
+		gt.println("How have you been? do you want to have a drink?");
+
+		// These line show use of if else use in Interger Variable to determine if the
+		// can afford to have some drinks with me.
+
+		if (userFunds <= 10) {
+			gt.println("Oft! its doesn't looks like you have much money should you be drinking?");
+		}
+		if (userFunds >= 10 && (userFunds <= 50)) {
+			gt.println("You can definitely afford to have a few drinks with me.");
+		} else {
+			gt.println("Lucky you, you can drink all night with me.");
+		}
+
+		double howMany = (userFunds / beerCost);
+
+		howMany = Math.round(howMany);
 		
-	if (a == 1 ) { 
-		gt.println("Correct");
 		
-	}if (a == 2) { 
-		gt.println("you");
-		
-	}if (a == 3) { 
-		gt.println("smell");
-		
-	}if (a == 4) { 
-		gt.println("really");
-		
-	}if (a == 5) { 
-		gt.println("bad");
-		
-	}if (a == 6) { 
-		gt.println("yey");
-		
-	}
-	*/
-		//gt.showHelp();
+		gt.println("You can have " + (howMany) + " drinks with me.");
+
+		gt.println("" + doYouHaveTime);
+
+		/*
+		 * int a = Integer.parseInt(gt.getInputString("select a number betw 1 and 6"));
+		 * 
+		 * if (a == 1 ) { gt.println("Correct");
+		 * 
+		 * }if (a == 2) { gt.println("you");
+		 * 
+		 * }if (a == 3) { gt.println("smell");
+		 * 
+		 * }if (a == 4) { gt.println("really");
+		 * 
+		 * }if (a == 5) { gt.println("bad");
+		 * 
+		 * }if (a == 6) { gt.println("yey");
+		 * 
+		 * }
+		 */
+		// gt.showHelp();
 	}
 }
--- test/src/PLayAround.java
+++ test/src/PLayAround.java
@@ -4,19 +4,15 @@ public static void main (String[]args) {
 
 		GTerm gt = new GTerm (500,500);
 		
-		int b = 5;
-	
-		int a = Integer.parseInt(gt.getInputString("Enter a numerical Value lower than 5"));
+		double val = 5.7483;
+		val = val*100;
+		val = Math.round(val);
+		val = val /100;
+
+
+		
+		System.out.println(val);
 		
 		
-		
-	if (a <= b) { 
-		gt.showMessageDialog("Correct");
-		
-	}
-	else { 
-		gt.showMessageDialog("Wrong");
-	}
-	
 	}
 }
