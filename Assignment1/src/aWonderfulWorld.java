f37de9e79ef1f2a4819ef914c3358a41b1fb5546
Rustynails93 <68316929+Rustynails93@users.noreply.github.com>
2020-09-20 20:59:06 +1000

    added new imagews for menu box and cafe

--- Assignment1/src/aWonderfulWorld.java
+++ Assignment1/src/aWonderfulWorld.java
@@ -11,8 +11,8 @@ public class aWonderfulWorld {
 		gt.setFontColor(Color.white);
 		
 		// Print line String greeting
-		String greeting = "Hello there traveller!" + "\n" + 		
-				"I need your help!" + "\n" + "Please will you tell me your name?";	
+		String greeting = "Hey, you there!" + "\n" + 		
+			"Will you tell me your name?";	
 		gt.println (greeting);
 		
 		//Set a dialog box so that text can be seen clearly
@@ -29,18 +29,18 @@ public class aWonderfulWorld {
 	
 		//Create string to be defined by used input. As it is a name, an undefined amount of charcters and symbols 
 		//a String is the only option
-		String [] playerName;
+		String playerName;
 		playerName = gt.getInputString("What was your name?");
-		playerName[0].toUpperCase() = playerName;
+		
 		//This Code Block is used to refresh the page to display a new output,
 		// which uses the players input to greet them in playerName
-		{
+		
 			gt.clear();
 	
 		gt.setXY(100,200);
-		gt.setFontSize(25);
+		gt.setFontSize(20);
 		gt.setFontColor(Color.white);
-		gt.println(playerName + "! What a great name!" + "\n" + "Can you help me save the princess?");
+		gt.println(playerName + "! What a great name!" + "\n" + "Do you want to come on an"  + "\n" +  "adventure with me?");
 	
 		gt.setXY(80, 190);
 		gt.addImageIcon("lib/dialogBox.png");
@@ -53,7 +53,7 @@ public class aWonderfulWorld {
 		
 		String pleaseHelp = gt.getInputString("Will you help me?");
 		pleaseHelp = pleaseHelp.toLowerCase();
-
+		
 		
 		
 		//This code block is not checking for a Boolean variable from the the input the user gives us
@@ -67,11 +67,12 @@ public class aWonderfulWorld {
 		gt.setFontSize(25);
 		gt.setFontColor(Color.white);
 		
-		gt.println("I'm so happy that you can help me.");
+		gt.println("I'm so happy that you can hang out.");
 		
-		gt.setXY(510, 275);
-		gt.setFontSize(8);
-		gt.println("click to continue");
+		gt.setFontSize(20);
+		gt.println("There are three different paths to take" + "\n" +  
+		"Which one should we take?");
+		
 		
 		gt.setXY(80, 190);
 		gt.addImageIcon("lib/dialogBox.png");
@@ -82,20 +83,73 @@ public class aWonderfulWorld {
 		gt.setXY(0, 0);
 		gt.addImageIcon("lib/woods.jpg");
 		
+		// user input defines a set of three possible char inputs through converting string to Char
+		//each with there own if statements
+		//
+		String userChoice = gt.getInputString("Which path do you want to take? A, B or C");
+		char pathChoice =  userChoice.charAt(0);
+		
+		if (pathChoice == 'A' || (pathChoice == 'a')) {
+			gt.clear();
+			
+			gt.setXY(95,200);
+			gt.setFontSize(18);
+			gt.setFontColor(Color.white);
+			gt.println("This cafe looks like a nice spot." +"\n" + "Did you want to get something to eat or drink?" + "\n"
+			+ "Did you bring your wallet?");
+				
+		
+			gt.setXY(80, 190);
+			gt.addImageIcon("lib/dialogBox.png");
+
+			
+			gt.setXY(0, 0);
+			gt.addImageIcon("lib/Cafe.png");
+			
+			int wallet = Integer.parseInt(gt.getInputString("How much do you have?"));
+					
+			gt.clear();
+		
+			gt.setXY(80, 190);
+			gt.addImageIcon("lib/menuBox.png");
+			double coffee;
+			double croissant;
+			double slice;
+			double milk;
+			double tea;
+				
+			gt.setXY(0, 0);
+			gt.addImageIcon("lib/Cafe.png");
+			
+			//gt.setXY(0, 0);
+			//gt.addImageIcon("lib/Cafe.png");
+		}
+		if (pathChoice == 'B'|| (pathChoice == 'b' )) {
+			gt.clear();
+			
+			
+			gt.setXY(0, 0);
+			gt.addImageIcon("Cafe.png");
+		}
+		if (pathChoice == 'C' || (pathChoice == 'c' ) ) {
+			gt.clear();
+			
+			
+			gt.setXY(0, 0);
+			gt.addImageIcon("Cafe.png");
+		}
+		
+		
 		
 	}
 	
-	else if (pleaseHelp.equals("no"))
-	{ 
+	else if (pleaseHelp.equals("no")){ 
 		gt.setXY(100,200);
 		gt.setFontSize(25);
 		gt.setFontColor(Color.white);
 		
-		gt.println("It will really help the kingdom." + "\n" + "You should come " + playerName);
-		
-		gt.setXY(510, 275);
-		gt.setFontSize(8);
-		gt.println("click to continue");
+		gt.println("That's a shame" + "\n" + "Guess we will hand out next time " + playerName);
+
 		
 		gt.setXY(80, 190);
 		gt.addImageIcon("lib/dialogBox.png");
@@ -106,13 +160,12 @@ public class aWonderfulWorld {
 		gt.setXY(0, 0);
 		gt.addImageIcon("lib/woods.jpg");
 		
-	
-	}
-	// else 
-	// {
 	}
 		
 		
 		// gt.showHelp();
 	}
 }
+	
+
+
