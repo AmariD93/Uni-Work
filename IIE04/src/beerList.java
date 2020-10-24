4818aac7c107d60685a2a6e285ce235a2fb68335
Rustynails93 <68316929+Rustynails93@users.noreply.github.com>
2020-10-24 11:14:51 +1100

    sewnd

--- IIE04/src/beerList.java
+++ IIE04/src/beerList.java
@@ -10,80 +10,74 @@ public class beerList {
 		gt.setFontStyle(5);
 		gt.setFontColor(Color.black);
 		gt.setFontSize(18);
-		
-		//Variable intilisation
-		char beerScore;
-		String beerName;
-		int beerStock = 0;
-		double beerCost;
-		boolean beerGood;
-		
+
+		int beerNumber = Integer.parseInt(gt.getInputString("How many beers do you want to review?"));
+
+		String[] beerNames = new String[beerNumber];
+		char[] beerScores = new char[beerNumber];
+		int[] beersStock = new int[beerNumber];
+		double[] beerCosts = new double[beerNumber];
+		boolean[] beersGood = new boolean[beerNumber];
+
 		String columns;
-		columns = "Beer name\tRating\tStock\tCost\t";		
+		columns = "Beer name\tRating\tStock\tCost\t";
 		gt.addTable(400, 300, columns);
 		gt.println("");
-		//Intitialistion of Integer variable and user input for how many times this program loops
-		int beerNumber =Integer.parseInt(gt.getInputString("How many beers do you want to review?"));
+
 		double sum = 0;
-		int stop = 0;
-		//initilising while method using Integer variables created
-		while(stop<beerNumber) {
-		//creating user input
-		String userInput = gt.getInputString(
-				"Beer " + (stop + 1) + "\nBeers name, Score for A to F,"
-				+ " How many you have in stock, How much does the beer cost?, Have you drank this beer?");
-		String [] userAnswer= userInput.split(", ");
-		
-		//using .split to create string variable	
-		beerName= userAnswer[0];
-		//Using while statement to create a requirement for the input
-		//condition is that it must not be blank. Alternatively, you can
-		//set .length(x) to create a condition that i must have x amount of
-		// characters
-			while(beerName.isBlank())
+		int x = 0;
+
+		while (x < beerNumber) {
+
+			String userInput = gt.getInputString("Beer " + (stop + 1) + "\nBeers name, Score for A to F,"
+					+ " How many you have in stock, How much does the beer cost?, Have you drank this beer?");
+			String[] userAnswer = userInput.split(", ");
+
+			beerName = userAnswer[0];
+
+			while (beerName.isBlank())
 				beerName = gt.getInputString("Please enter the beers name.");
-		// using .split to create Char variable
-		beerScore = userAnswer[1].charAt(0);
-		//Using while statement to create a requirement for the input.
-		//condition is that it is an alphabetical character.
-			while(Character.isAlphabetic(beerScore) == false)
-					beerScore = gt.getInputString("Please re-enter a score from A to F").charAt(0);
-		// using .split to create Integer variable				
-		beerStock = Integer.parseInt(userAnswer[2]);
-		//Using while statement to create a requirement for the input
-		//condition is that It must numerically be more than 0. Alternatively, you can
-		//set a limit to what the use can enter.
-			while(beerStock <=0)
-				beerStock = Integer.parseInt(gt.getInputString("Please enter how many " + beerName + " you have in stock"));
-		//using .split to create double variable	
-		beerCost = Double.parseDouble(userAnswer[3]);
-		//Using while statement to create a requirement for the input
-		//condition is that It must numerically be more than 0. Alternatively, you can
-		//set a limit to what the use can enter.
-			while(beerCost <= 0)			
+
+			beerScore = userAnswer[1].charAt(0);
+
+			while (Character.isAlphabetic(beerScore) == false)
+				beerScore = gt.getInputString("Please re-enter a score from A to F").charAt(0);
+
+			beerStock = Integer.parseInt(userAnswer[2]);
+
+			while (beerStock <= 0)
+				beerStock = Integer
+						.parseInt(gt.getInputString("Please enter how many " + beerName + " you have in stock"));
+
+			beerCost = Double.parseDouble(userAnswer[3]);
+
+			while (beerCost <= 0)
 				beerCost = Double.parseDouble(gt.getInputString("Please Re-enter cost of the beer."));
-			//adding beerCost to sum to calculate average
+
 			sum += beerCost;
-		//using .split to create boolean variable
-		beerGood = Boolean.parseBoolean(userAnswer[4]);
-		//Using while statement to create a requirement for the input
-		//condition is that it must be true. Alternatively, you can
-		//use (beerGood == false)
-			while(!beerGood)
-				beerGood = Boolean.parseBoolean(gt.getInputString("If you haven't Drank the beer what are you rating is. Please enter true to continue"));
-			
-		// using string variable to create message output	 
-		String message = beerName + "\t " +  beerScore +"\t " + 
-			beerStock + "\t " + "$"+ beerCost ;
-		//gt.println(message);
-		gt.addRowToTable(0, message);
-		stop+=1;
-		
+
+			beerGood = Boolean.parseBoolean(userAnswer[4]);
+
+			while (!beerGood)
+				beerGood = Boolean.parseBoolean(gt.getInputString(
+						"If you haven't Drank the beer what are you rating is. Please enter true to continue"));
+
+			beerNames[x] = beerName;
+			beerScores[x] = beerScore;
+			beersStock[x] = beerStock;
+			beerCosts[x] = beerCost;
+			beersGood[x] = beerGood;
+
+			String message = beerNames + "\t " + beerScores + "\t " + beersStock + "\t " + "$" + beerCosts;
+
+			gt.addRowToTable(0, message);
+
+			x += 1;
 
 		}
 		double stockAverage;
 		stockAverage = sum / beerNumber;
-		
+
 		gt.println("Your cost average is " + stockAverage);
 		// gt.showHelp();
 
--- IIE05/src/beerArray.java
+++ IIE05/src/beerArray.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class beerArray {
 	public static void main(String[] args) {
-		GTerm gt = new GTerm(420, 350);
+		GTerm gt = new GTerm(420, 400);
 
 		// Test input : VB, A, 5, 4.55, true
 
@@ -12,82 +12,108 @@ public class beerArray {
 		gt.setFontColor(Color.black);
 		gt.setFontSize(18);
 
-		// Variable intilisation
+		int beerNumber = Integer.parseInt(gt.getInputString("How many beers do you want to review?"));
+
+		String[] beerNames = new String[beerNumber];
+		char[] beerScores = new char[beerNumber];
+		int[] beersStock = new int[beerNumber];
+		double[] beerCosts = new double[beerNumber];
+		boolean[] beersGood = new boolean[beerNumber];
+
+		String beerName;
+		char beerScore;
+		int beerStock;
+		double beerCost;
+		boolean beerGood;
 
 		String columns;
 		columns = "Beer name\tRating\tStock\tCost\t";
 		gt.addTable(400, 300, columns);
 		gt.println("");
 
-		int beerNumber = Integer.parseInt(gt.getInputString("How many beers do you want to review?"));
-
-		String[] beerName = new String[beerNumber];
-		char[] beerScore = new char[beerNumber];
-		int[] beerStock = new int[beerNumber];
-		double[] beerCost = new double[beerNumber];
-		boolean[] beerGood = new boolean[beerNumber];
-
 		double costAverage;
-		int stockAverage;
 		double sumC = 0;
-		int sumS = 0;
-		
 		int x = 0;
+		int minArrayIndex=0;
+
 		while (x < beerNumber) {
+			String userInput = gt.getInputString("Beer " + (x + 1) + "\nBeers name, Score for A to F,"
+					+ " How many you have in stock, How much does the beer cost?, Have you drank this beer?");
 
-			beerName[x] = gt.getInputString("Beer " + (x + 1) + "\nEnter Beer name");
-			while (beerName[x].isBlank()) {
-				beerName[x] = gt.getInputString("Please enter a name for Beer " + (x + 1));
-			}
+			String[] userAnswer = userInput.split(", ");
 
-			beerScore[x] = gt.getInputString(beerName[x] + "\nEnter score A to F for " + beerName[x]).charAt(0);
-			while (Character.isAlphabetic(beerScore[x]) == false) {
-				beerScore[x] = gt.getInputString("Please enter a Score for " + beerName[x]).charAt(0);
-			}
+			beerName = userAnswer[0];
 
-			beerStock[x] = Integer
-					.parseInt(gt.getInputString(beerName[x] + "\nEnter amount of " + beerName[x] + " stock"));
-			while (beerStock[x] < 0) {
-				beerStock[x] = Integer
-						.parseInt(gt.getInputString("Please enter how many " + beerName[x] + "'s you have"));
-			}
+			while (beerName.isBlank())
+				beerName = gt.getInputString("Please enter the beers name.");
 
-			beerCost[x] = Double.parseDouble(gt.getInputString(beerName[x] + "\nEnter cost of " + beerName[x]));
-			while (beerStock[x] < 0) {
-				beerName[x] = gt.getInputString("Please enter how many " + beerName[x] + "'s you have");
-			}
+			beerScore = userAnswer[1].charAt(0);
 
-			beerGood[x] = Boolean
-					.parseBoolean(gt.getInputString(beerName[x] + "\nHave you tried " + beerName[x] + "?"));
-			while (!beerGood[x]) {
-				beerGood[x] = Boolean
-						.parseBoolean(gt.getInputString("You must have tried " + beerName[x] + "to rate it"));
-			}
+			while (Character.isAlphabetic(beerScore) == false)
+				beerScore = gt.getInputString("Please re-enter a score from A to F").charAt(0);
+
+			beerStock = Integer.parseInt(userAnswer[2]);
+
+			while (beerStock <= 0)
+				beerStock = Integer
+						.parseInt(gt.getInputString("Please enter how many " + beerName + " you have in stock"));
+
 			
-		
-			sumC += beerCost[x];
-			sumS += beerStock[x];
-		
+			beerCost = Double.parseDouble(userAnswer[3]);
+
+			while (beerCost <= 0)
+				beerCost = Double.parseDouble(gt.getInputString("Please Re-enter cost of the beer."));
+
+			beerGood = Boolean.parseBoolean(userAnswer[4]);
+
+			while (!beerGood)
+				beerGood = Boolean.parseBoolean(gt.getInputString(
+						"If you haven't Drank the beer what are you rating is. Please enter true to continue"));
+
+			beerNames[x] = beerName;
+			beerScores[x] = beerScore;
+			beersStock[x] = beerStock;
+			beerCosts[x] = beerCost;
+			beersGood[x] = beerGood;
+						
+			if (beersStock[x] < beersStock[minArrayIndex])
+				minArrayIndex = x;
+
+			String message = beerNames[x] + "\t " + beerScores[x] + "\t " + beersStock[x] + "\t " + "$" + beerCosts[x];
+
+			gt.addRowToTable(0, message);
+
+			sumC += beerCosts[x];
+
 			x += 1;
+
 		}
-	
-		while (x < beerNumber) {
-		String message = beerName[x] + "\t" + beerScore[x] + "\t" + beerStock[x] + "\t" + beerCost[x];
-		gt.addRowToTable(0, message);	
-		x+=1;
-		}
+
+		String lowestBeer = beerNames[minArrayIndex] + "\t " + beerScores[minArrayIndex] + "\t "
+				+ beersStock[minArrayIndex] + "\t " + "$" + beerCosts[minArrayIndex];
+
 		costAverage = sumC / beerNumber;
-		stockAverage = sumS / beerNumber;
 		gt.println("Your cost average is " + costAverage);
-		gt.println("Your stock average is " + stockAverage);
-		
-		
-		Arrays.sort(beerStock);
-		gt.println("The lowest stock you have is " + beerStock[0] );
-		gt.println("The highest stock you have is " + beerStock[beerStock.length-1]);
 
+		Arrays.sort(beersStock);
+		double median;
+		int sumOfEvenMedian = 0;
+		int arrayLength = beersStock.length;
+
+		if (arrayLength % 2 == 0) {
+			sumOfEvenMedian = beersStock[arrayLength / 2] + beersStock[arrayLength / 2 - 1];
+		median = ((double) sumOfEvenMedian) / 2;
+		}
+		else
+		     median = (double) beersStock[beersStock.length/2];
 		
-	
+		gt.println("median is: " + median);
+		
+		gt.showMessageDialog("Press ok to see the beer with lowest stock");
+		gt.clearRowsOfTable(0);
+		gt.addRowToTable(0, lowestBeer);
+
+		// gt.showHelp();
+
 	}
-
 }
